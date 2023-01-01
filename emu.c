614c0586d922ffce86cb54a1894c0489008c9610
jackdonofrio <john.a.donofrio.25@dartmouth.edu>
2023-01-01 15:52:08 -0500

    added nonblocking func

--- emu.c
+++ emu.c
@@ -176,6 +176,29 @@ uint8_t keypress_block(void)
     return c;
 }
 
+uint8_t keypress_nonblock(void)
+{
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if(ch != EOF)
+    {
+        return (uint8_t) ch;
+    }
+    return 0;
+}
+
 /*
 ============================
 | State-handling functions |
